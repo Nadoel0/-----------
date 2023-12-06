0de646241b211cc3ebab28def057515b189b7b00
Nadoel0 <irek-ignatev@mail.ru>
2023-12-06 19:38:20 +0300

    timer

---
+++ Задание_JS/script.js
@@ -0,0 +1,48 @@
+const timerElement = document.getElementById("timer");
+const timerIcon = document.getElementById("timerIcon");
+const hoursElement = document.getElementById("hours");
+const timerButton = document.getElementById("timerButton");
+
+let timer;
+let duration = 0;
+let timerLeft;
+
+function startTimer() {
+  duration = parseInt(hoursElement.value) * 3600;
+
+  if (isNaN(duration) || duration <= 0) {
+    alert("Введите корректное число");
+    return;
+  }
+
+  timerLeft = duration;
+
+  timer = setInterval(updateTimer, 1000);
+}
+
+function updateTimer() {
+  if (timerLeft > 0) {
+    timerLeft--;
+    const precRem = ((duration - timerLeft) / duration) * 100;
+    const dashOffset = (precRem / 100) * 282;
+    console.log(dashOffset);
+    const timerPath = `<circle class="timer-path" cx="50%" cy="50%" r="45%" stroke-dasharray="282" stroke-dashoffset="${dashOffset}" />`;
+
+    const hours = formatNumber(Math.floor(timerLeft / 3600));
+    const minutes = formatNumber(Math.floor((timerLeft % 3600) / 60));
+    const seconds = formatNumber(timerLeft % 60);
+
+    timerIcon.innerHTML = timerPath;
+    timerElement.innerText = `${hours}:${minutes}:${seconds}`;
+  } else {
+    clearInterval(timer);
+    timerIcon = "";
+    alert("Ваше время вышло");
+  }
+}
+
+function formatNumber(num) {
+  return num < 10 ? `0${num}` : num;
+}
+
+timerButton.addEventListener("click", startTimer);
